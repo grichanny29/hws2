2d70a548c1c018773186ac8f2d17afa73524d095
alex <alex.grich84@gmail.com>
2026-05-27 19:31:59 +0300

    дз

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -69,3 +69,212 @@ const HW1 = () => {
 }
 
 export default HW1
+
+
+// Что нужно написать вместо XXX, чтобы кнопка отправки комментария отрабатывала верно:
+
+// первоначально кнопка должна быть в состоянии disable, а после успешного выполнения условия (комментарий должен быть больше 5 символов) должна раздизаблиться.
+// ❗ Ответ необходимо дать на основании данных (переменных), которые уже есть в коде
+
+
+
+// import { ChangeEvent, useState } from "react"
+// import { createRoot } from "react-dom/client"
+// const MIN_COMMENT_SIZE = 5
+// const LongCommentChecker = () => {
+//   const [comment, setComment] = useState<string>("")
+//   const isCommentReady = comment.length > MIN_COMMENT_SIZE
+//   const setCommentHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+//     const newComment = e.currentTarget.value
+//     setComment(newComment)
+//   }
+//   const clearCommentsHandler = () => {
+//     if (isCommentReady) {
+//       setComment("")
+//     }
+//   }
+//   return (
+//     <main>
+//       <textarea
+//         placeholder={"Your comment must have more than 5 charters"}
+//         value={comment}
+//         onChange={setCommentHandler}
+//       />
+//       <div>
+//         <button disabled={XXX} onClick={clearCommentsHandler}>
+//           Send comment
+//         </button>
+//       </div>
+//     </main>
+//   )
+// }
+// createRoot(document.getElementById("root")!).render(<LongCommentChecker />)
+
+
+
+// Какое значение будет выведено в консоль?
+
+// const student = {
+//   name: "Leonid",
+//   age: 64,
+//   friends: ["Masha", "Igor", "Yana"]
+// }
+// export const updatedStudent = {...student}
+// updatedStudent.friends.push("Liza")
+// console.log(student.friends[3])
+
+
+
+// Какое значение получит переменная "myFriendName"?
+
+// const student = {
+//   name: "Vladimir",
+// }
+// const newStudent = student
+// const myFriend = {
+//   ...newStudent,
+// }
+// const newUser = {
+//   name: "Lev",
+// }
+// export const myFriendName = student.name !== myFriend.name ? newUser.name : student.name
+// // Какое значение получит переменная "myFriendName"?
+
+
+// Что надо написать вместо XXX, чтобы при клике кнопка становилась красной?
+
+// import { useState } from "react"
+// import { createRoot } from "react-dom/client"
+// const ColorButton = () => {
+//   const [isColored, setIsColored] = useState<boolean>(false)
+//   return (
+//     <button style={{ backgroundColor: `${XXX === true ? "red" : ""}` }} onClick={() => setIsColored(true)}>
+//       Меняю цвет по клику
+//     </button>
+//   )
+// }
+// createRoot(document.getElementById("root")!).render(<ColorButton />)
+
+
+
+
+// Те пользователи, у которых в стэке пять и более технологий, должны в списке быть отмечены, как готовые к работе. Что надо вставить вместо XXX, чтобы код работал, как необходимо?
+
+// import { useState } from "react"
+// import { createRoot } from "react-dom/client"
+// type User = {
+//   id: number
+//   name: string
+//   personalData: PersonalData
+// }
+// type PersonalData = {
+//   gender: string
+//   age: number
+//   technologies: Technologies[]
+// }
+// type Technologies = "HTML" | "CSS" | "React" | "JS/TS" | "Redux"
+// const UsersList = () => {
+//   const [users] = useState<User[]>([
+//     {
+//       id: 1,
+//       name: "Bob",
+//       personalData: {
+//         gender: "male",
+//         age: 23,
+//         technologies: ["HTML", "CSS", "React", "JS/TS", "Redux"],
+//       },
+//     },
+//     {
+//       id: 2,
+//       name: "Alex",
+//       personalData: {
+//         gender: "male",
+//         age: 21,
+//         technologies: ["HTML", "CSS", "React"],
+//       },
+//     },
+//     {
+//       id: 3,
+//       name: "Ann",
+//       personalData: {
+//         gender: "female",
+//         age: 26,
+//         technologies: ["HTML", "CSS", "JS/TS"],
+//       },
+//     },
+//     {
+//       id: 4,
+//       name: "Helen",
+//       personalData: {
+//         gender: "female",
+//         age: 31,
+//         technologies: ["HTML", "CSS"],
+//       },
+//     },
+//     {
+//       id: 5,
+//       name: "Donald",
+//       personalData: {
+//         gender: "male",
+//         age: 28,
+//         technologies: ["React", "JS/TS", "Redux"],
+//       },
+//     },
+//   ])
+//   return (
+//     <ul>
+//       {users.map((u) => {
+//         return u.XXX.length >= 5 ? (
+//           <li key={u.id}>{`User ${u.name}. ${u.personalData.age}. Ready to work.`}</li>
+//         ) : (
+//           <li key={u.id}>{`User ${u.name}. ${u.personalData.age}. `}</li>
+//         )
+//       })}
+//     </ul>
+//   )
+// }
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+
+// Что надо вставить вместо XXX, чтобы код работал нормально?
+
+// import { useState } from "react"
+// import { createRoot } from "react-dom/client"
+// const PasswordChecker = () => {
+//   const [password, setPassword] = useState<string>("")
+//   return (
+//     <main>
+//       <p>Your password must have more than 8 charters!</p>
+//       <input
+//         placeholder={"Enter your password"}
+//         value={password}
+//         onChange={(e) => setPassword(e.currentTarget.value)}
+//         type={"password"}
+//       />
+//       {XXX < 9 && <p style={{ color: "red" }}>The password is too short!</p>}
+//     </main>
+//   )
+// }
+// createRoot(document.getElementById("root")!).render(<PasswordChecker />)
+
+
+
+// Чему равно copyUser.name после выполнения этого кода?
+
+// const user = {
+//   name: "Katya",
+//   age: 18
+// }
+// export const copyUser = {...user}
+// user.name = "Konstantin"
+
+
+// Чему равно значение copyStudent.age после выполнения этого кода?
+
+// const student = {
+//     name: "Nikita",
+//     age: 82
+// }
+// export const copyStudent = student
+ 
+// student.age = 16
--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -17,17 +17,18 @@ import s2 from '../../s1-main/App.module.css'
 * */
 
 // types
-export type AffairPriorityType = any // need to fix any
+export type AffairPriorityType =  'high' | 'middle' | 'low'
+
 export type AffairType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: number
+    name: string
     priority: AffairPriorityType
 }
 export type FilterType = 'all' | AffairPriorityType
 
 // constants
-const defaultAffairs: any = [ // need to fix any
-    {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
+const defaultAffairs:AffairType[]= [ 
+    {_id: 1, name: 'React', priority: 'high'}, 
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
     {_id: 4, name: 'work', priority: 'high'},
@@ -35,22 +36,25 @@ const defaultAffairs: any = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
+export const filterAffairs = (affairs:AffairType[], filter: FilterType): AffairType[] => {
+    if(filter==="all") {
+        return affairs// need to fix any
+    }
 
-
-    return affairs // need to fix
+    return affairs .filter(a=> a.priority===filter)// need to fix
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
+export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => { // need to fix any
 
-    return affairs // need to fix
+    return affairs.filter(a=>a._id !== _id) // need to fix
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) // need to fix any
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => { // need to fix any
+    const deleteAffairCallback = (_id:number) => {
+        setAffairs(deleteAffair(affairs,_id)) // need to fix any
         // need to fix
     }
 
